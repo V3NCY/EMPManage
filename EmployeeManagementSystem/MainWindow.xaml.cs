dc94a5b3fccacc9afac87d389c4bfb41acf5924c
V3NCY <venceslavam@gmail.com>
2024-06-25 17:20:02 +0300

    add employee window

--- EmployeeManagementSystem/MainWindow.xaml.cs
+++ EmployeeManagementSystem/MainWindow.xaml.cs
@@ -9,30 +9,61 @@ namespace EmployeeManagementSystem
             InitializeComponent();
         }
 
+        // Document Request Buttons
         private void OnSickLeaveRequestsButtonClick(object sender, RoutedEventArgs e)
         {
             SickLeaveWindow sickLeaveWindow = new SickLeaveWindow();
             sickLeaveWindow.ShowDialog();
-            DocumentsToggleButton.IsChecked = false; 
+            DocumentsToggleButton.IsChecked = false;
         }
 
         private void OnOtherLeaveRequestsButtonClick(object sender, RoutedEventArgs e)
         {
             OtherLeaveWindow specialRequestsWindow = new OtherLeaveWindow();
             specialRequestsWindow.ShowDialog();
-            DocumentsToggleButton.IsChecked = false; 
+            DocumentsToggleButton.IsChecked = false;
         }
 
         private void OnPaidLeaveRequestsButtonClick(object sender, RoutedEventArgs e)
         {
             PaidLeaveWindow paidLeaveWindow = new PaidLeaveWindow();
             paidLeaveWindow.ShowDialog();
-            DocumentsToggleButton.IsChecked = false; 
+            DocumentsToggleButton.IsChecked = false;
         }
 
+        // Employee Management Buttons
+        private void OnAddEmployeeButtonClick(object sender, RoutedEventArgs e)
+        {
+            AddEmployeesWindow addEmployeesWindow = new AddEmployeesWindow();
+            addEmployeesWindow.ShowDialog();
+            EmployeesToggleButton.IsChecked = false;
+        }
+
+        private void OnEmployeeListButtonClick(object sender, RoutedEventArgs e)
+        {
+            EmployeeListWindow employeeListWindow = new EmployeeListWindow();
+            employeeListWindow.ShowDialog();
+            EmployeesToggleButton.IsChecked = false;
+        }
+
+        private void OnArchiveListButtonClick(object sender, RoutedEventArgs e)
+        {
+            ArchiveListWindow archiveListWindow = new ArchiveListWindow();
+            archiveListWindow.ShowDialog();
+            EmployeesToggleButton.IsChecked = false;
+        }
+
+        private void OnPersonalFilesButtonClick(object sender, RoutedEventArgs e)
+        {
+            PersonalFilesWindow personalFilesWindow = new PersonalFilesWindow();
+            personalFilesWindow.ShowDialog();
+            EmployeesToggleButton.IsChecked = false;
+        }
+
+        // ToggleButton Checked and Unchecked Handlers
         private void DocumentsToggleButton_Checked(object sender, RoutedEventArgs e)
         {
-            CloseAllPopups();
+            CloseEmployeesPopup();
             DocumentsPopup.IsOpen = true;
         }
 
@@ -41,9 +72,31 @@ namespace EmployeeManagementSystem
             DocumentsPopup.IsOpen = false;
         }
 
+        private void EmployeesToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            CloseDocumentsPopup();
+            EmployeesPopup.IsOpen = true;
+        }
+
+        private void EmployeesToggleButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            EmployeesPopup.IsOpen = false;
+        }
+
         private void CloseAllPopups()
         {
             DocumentsPopup.IsOpen = false;
+            EmployeesPopup.IsOpen = false;
+        }
+
+        private void CloseDocumentsPopup()
+        {
+            DocumentsPopup.IsOpen = false;
+        }
+
+        private void CloseEmployeesPopup()
+        {
+            EmployeesPopup.IsOpen = false;
         }
     }
 }
