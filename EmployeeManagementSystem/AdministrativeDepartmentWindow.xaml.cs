e6330b2c96cfc8f080f7fa5e61671159023fa6e6
V3NCY <venceslavam@gmail.com>
2024-08-14 11:15:46 +0300

    content control added for inwindow opening

--- EmployeeManagementSystem/AdministrativeDepartmentWindow.xaml.cs
+++ EmployeeManagementSystem/AdministrativeDepartmentWindow.xaml.cs
@@ -1,27 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace EmployeeManagementSystem
 {
-    /// <summary>
-    /// Interaction logic for AdministrativeDepartmentWindow.xaml
-    /// </summary>
     public partial class AdministrativeDepartmentWindow : Window
     {
         public AdministrativeDepartmentWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnPersonalFilesButtonClick(object sender, RoutedEventArgs e)
+        {
+            PersonalFilesWindow personalFilesWindow = new PersonalFilesWindow();
+
+            MainContentControl.Content = personalFilesWindow.Content;
         }
     }
 }
