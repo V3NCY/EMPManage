cb0ac8baa44bc71aa28e6358c55f62e82f9c240a
V3NCY <venceslavam@gmail.com>
2025-01-09 16:47:24 +0200

    administration window button active added

--- EmployeeManagementSystem/AdministrativeDepartmentWindow.xaml.cs
+++ EmployeeManagementSystem/AdministrativeDepartmentWindow.xaml.cs
@@ -1,82 +1,100 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace EmployeeManagementSystem
 {
     public partial class AdministrativeDepartmentWindow : Window
     {
+        private Button _activeButton;
+
         public AdministrativeDepartmentWindow()
         {
             InitializeComponent();
         }
 
-        private void OnPersonalFilesButtonClick(object sender, RoutedEventArgs e)
+        private void SetActiveButton(Button button)
         {
-            PersonalFilesWindow personalFilesWindow = new PersonalFilesWindow();
+            // Reset the style of the previously active button
+            if (_activeButton != null)
+            {
+                _activeButton.ClearValue(Button.BackgroundProperty);
+                _activeButton.ClearValue(Button.ForegroundProperty);
+            }
 
-            MainContentControl.Content = personalFilesWindow.Content;
-        }
-
-        private void OnContractListButtonClick(object sender, RoutedEventArgs e)
-        {
-            ContractListWindow contractListsWindow = new ContractListWindow();
-
-            MainContentControl.Content = contractListsWindow.Content;
+            // Set the new active button and update its style
+            _activeButton = button;
+            _activeButton.Background = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(7, 55, 99)); // #073763
+            _activeButton.Foreground = System.Windows.Media.Brushes.White;
         }
 
         private void OnDocumentsListButtonClick(object sender, RoutedEventArgs e)
         {
+            SetActiveButton(sender as Button);
             DocumentsListWindow documentsListsWindow = new DocumentsListWindow();
-
             MainContentControl.Content = documentsListsWindow.Content;
+        }
+
+        private void OnPersonalFilesButtonClick(object sender, RoutedEventArgs e)
+        {
+            SetActiveButton(sender as Button);
+            PersonalFilesWindow personalFilesWindow = new PersonalFilesWindow();
+            MainContentControl.Content = personalFilesWindow.Content;
         }
 
         private void OnEmployeeRewardsButtonClick(object sender, RoutedEventArgs e)
         {
+            SetActiveButton(sender as Button);
             EmployeeRewardsWindow employeeRewardsWindow = new EmployeeRewardsWindow();
-
             MainContentControl.Content = employeeRewardsWindow.Content;
         }
 
         private void OnProjectsAndTasksButtonClick(object sender, RoutedEventArgs e)
         {
+            SetActiveButton(sender as Button);
             ProjectsAndTasksWindow projectsAndTasksWindow = new ProjectsAndTasksWindow();
-
             MainContentControl.Content = projectsAndTasksWindow.Content;
         }
 
         private void OnFinanceAndBudgetButtonClick(object sender, RoutedEventArgs e)
         {
+            SetActiveButton(sender as Button);
             FinanceAndBudgetWindow financeAndBudgetWindow = new FinanceAndBudgetWindow();
-
             MainContentControl.Content = financeAndBudgetWindow.Content;
         }
 
         private void OnCommunicationEventsButtonClick(object sender, RoutedEventArgs e)
         {
+            SetActiveButton(sender as Button);
             CommunicationEventsWindow communicationEventsWindow = new CommunicationEventsWindow();
-
             MainContentControl.Content = communicationEventsWindow.Content;
         }
+
         private void OnEducationDevelopmentButtonClick(object sender, RoutedEventArgs e)
         {
+            SetActiveButton(sender as Button);
             EducationDevelopmentWindow educationDevelopmentWindow = new EducationDevelopmentWindow();
-
             MainContentControl.Content = educationDevelopmentWindow.Content;
         }
 
         private void OnRulesAndPolicyButtonClick(object sender, RoutedEventArgs e)
         {
+            SetActiveButton(sender as Button);
             RulesAndPolicyWindow rulesAndPolicyWindow = new RulesAndPolicyWindow();
-
             MainContentControl.Content = rulesAndPolicyWindow.Content;
         }
 
         private void OnMaintenanceAndInfrastructureButtonClick(object sender, RoutedEventArgs e)
         {
+            SetActiveButton(sender as Button);
             MaintenanceAndInfrastructureWindow miWindow = new MaintenanceAndInfrastructureWindow();
-
             MainContentControl.Content = miWindow.Content;
         }
 
+        private void OnContractListButtonClick(object sender, RoutedEventArgs e)
+        {
+            SetActiveButton(sender as Button);
+            ContractListWindow contractListsWindow = new ContractListWindow();
+            MainContentControl.Content = contractListsWindow.Content;
+        }
     }
 }
